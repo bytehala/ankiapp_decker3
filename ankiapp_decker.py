85e07bdae3a1a0a147b4c064f4b4bebd9a1a5146
Lem <bytehala@users.noreply.github.com>
2020-02-07 18:51:32 +0800

    renaming works

--- ankiapp_decker.py
+++ ankiapp_decker.py
@@ -1,31 +1,38 @@
 import os.path
 import csv
 
-images = os.scandir(os.getcwd() + "")# "/images")
+
+images = os.scandir(os.getcwd() + "/images")
 #print(images)
 sortedimages = sorted(images, key=lambda x: x.stat().st_mtime, reverse=False)
 newlist = []
 
 # Read the tsv
 def readTsv():
+	result = []
 	with open("Adjectives_Sample.tsv") as fd:
 		rd = csv.reader(fd, delimiter="\t")
+		
 		for row in rd:
-			print(row[3])
+#			print(row[3])
+			result.append(row[3])
+	return result[1:]
 
 names = ['one.png', 'two.png', 'three.png']
 def renameImages(names):
 	index = 0
 	for i in sortedimages:
-		print(i.name)
+		#print(i.name)
 	
-		if(i.name.endswith(".png")):
+		if(i.name.endswith(".jpeg")):
 			#print(i.stat().st_ctime)
-			print(i.stat())
+			#print(i.stat())
 			print(i.name)
 			print(names[index])
-	#		os.rename("" + i.name, "" + names[index])
+			dir = "images/"
+			os.rename("images/" + i.name, "images/" + names[index])
 			index = index + 1
 			
-readTsv()
-renameImages(names)
+test = readTsv()
+print(test)
+renameImages(test)
